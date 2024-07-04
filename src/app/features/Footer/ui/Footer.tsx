65d7f9e955762542bd4e5dd1b5845a1337288cd3
ktiukalov <tiukalov@realx.tech>
2024-07-04 17:31:01 +0300

    feat/tablet Footer, YMap

--- src/app/features/Footer/ui/Footer.tsx
+++ src/app/features/Footer/ui/Footer.tsx
@@ -1,93 +1,101 @@
 'use client'
-import { Logo } from '@/src/app/widgets/Logo'
+import {Logo} from '@/src/app/widgets/Logo'
 import s from './Footer.module.scss'
-import { Button, Flex, Text } from 'stone-kit'
-import { YandexMap } from '@/src/app/widgets/YandexMap'
-import { FormContext } from '@/src/app/providers/formProvider'
-import { useContext } from 'react'
-import { ModalForm } from '../../ModalForm'
+import {Button, Flex, Text} from 'stone-kit'
+import {YandexMap} from '@/src/app/widgets/YandexMap'
+import {FormContext} from '@/src/app/providers/formProvider'
+import {useContext} from 'react'
+import {ModalForm} from '../../ModalForm'
 
 interface IFooterProps {
-	className?: string
+    className?: string
 }
 
 export const Footer = ({}: IFooterProps) => {
-	const form = useContext(FormContext)
+    const form = useContext(FormContext)
 
-	const infoList = [
-		{
-			title: 'Единый номер',
-			text: '8 800 775 24 71',
-		},
-		{
-			title: 'Единая почта',
-			text: 'example@gmail.com',
-		},
-		{
-			title: 'Адрес',
-			text: 'Москва, ул. Пушкина, д 52',
-		},
-		{
-			title: 'Время работы',
-			text: 'ПН-ВС с 9:00 до 21:00',
-		},
-	]
+    const infoList = [
+        {
+            title: 'Единый номер',
+            text: '8 800 775 24 71',
+        },
+        {
+            title: 'Единая почта',
+            text: 'example@gmail.com',
+        },
+        {
+            title: 'Адрес',
+            text: 'Москва, ул. Пушкина, д 52',
+        },
+        {
+            title: 'Время работы',
+            text: 'ПН-ВС с 9:00 до 21:00',
+        },
+    ]
 
-	return (
-		<Flex
-			className={s.root}
-			id='contacts'>
-			<Flex className={s.logoWrapper}>
-				<Logo
-					variant='white'
-					between
-					uk
-				/>
-			</Flex>
-			<YandexMap />
-			<Flex className={s.infoList}>
-				{infoList.map((item, i) => {
-					return (
-						<Flex
-							key={i}
-							className={s.infoCard}>
-							<Text
-								className={s.infoTitle}
-								html={`${item.title}`}
-							/>
-							<Text
-								className={s.infoText}
-								html={`${item.text}`}
-							/>
-						</Flex>
-					)
-				})}
-			</Flex>
-			<Flex className={s.btnWrapper}>
-				<Button
-					as='link'
-					href='https://hh.ru'
-					variant='shade'
-					width='full'
-					size='large'>
-					Смотреть вакансии
-				</Button>
-				<Button
-					as='button'
-					variant='blue'
-					width='full'
-					size='large'
-					onClick={() => form?.setIsFormModalOpen(true)}>
-					Оставить обращение
-				</Button>
-			</Flex>
-			{form && (
-				<ModalForm
-					isFormOpen={form.isFormModalOpen}
-					setIsFormOpen={form?.setIsFormModalOpen}
-				/>
-			)}
-		</Flex>
-	)
+    return (
+        <Flex
+            className={s.root}
+            id='contacts'>
+            <Flex className={s.topHeader}>
+                <Flex className={s.logoWrapper}>
+                    <Logo
+                        variant='white'
+                        uk
+                    />
+                </Flex>
+
+                <Flex className={s.btnWrapper}>
+                    <Button
+                        as='link'
+                        href='https://hh.ru'
+                        variant='shade'
+                        width='full'
+                        size='large'>
+                        Смотреть вакансии
+                    </Button>
+                    <Button
+                        as='button'
+                        variant='blue'
+                        width='full'
+                        size='large'
+                        onClick={() => form?.setIsFormModalOpen(true)}>
+                        Оставить обращение
+                    </Button>
+                </Flex>
+            </Flex>
+
+            <Flex className={s.infoList}>
+                {infoList.map((item, i) => {
+                    return (
+                        <Flex
+                            key={i}
+                            className={s.infoCard}>
+                            <Text
+                                className={s.infoTitle}
+                                html={`${item.title}`}
+                            />
+                            <Text
+                                className={s.infoText}
+                                html={`${item.text}`}
+                            />
+                        </Flex>
+                    )
+                })}
+            </Flex>
+
+
+            <YandexMap/>
+
+            {form && (
+                <ModalForm
+                    isFormOpen={form.isFormModalOpen}
+                    setIsFormOpen={form?.setIsFormModalOpen}
+                />
+            )}
+
+
+        </Flex>
+    )
 }
 
